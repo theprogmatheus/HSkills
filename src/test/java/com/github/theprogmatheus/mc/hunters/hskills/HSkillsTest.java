dac46e10f09d0a8d88e15707cf47467301130978
theprogmatheus <theprog.matheus@gmail.com>
2025-08-15 10:43:58 -0300

    refactor test

--- src/test/java/com/github/theprogmatheus/mc/hunters/hskills/HSkillsTest.java
+++ src/test/java/com/github/theprogmatheus/mc/hunters/hskills/HSkillsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PluginTemplateTest {
+public class HSkillsTest {
 
     private ServerMock server;
     private HSkills plugin;
