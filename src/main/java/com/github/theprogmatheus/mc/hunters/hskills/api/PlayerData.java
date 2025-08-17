9784c4c3794356fe844b39ef42add6e8ce7732f9
theprogmatheus <theprog.matheus@gmail.com>
2025-08-16 21:36:48 -0300

    Create PlayerData

---
+++ src/main/java/com/github/theprogmatheus/mc/hunters/hskills/api/PlayerData.java
@@ -0,0 +1,86 @@
+package com.github.theprogmatheus.mc.hunters.hskills.api;
+
+import java.util.UUID;
+
+public interface PlayerData {
+
+    /**
+     * @return the player uuid
+     */
+    UUID getId();
+
+    /**
+     * @return the current exp
+     */
+    double getExp();
+
+    /**
+     * Define new exp to player
+     *
+     * @param exp - the new exp to define
+     */
+    void setExp(double exp);
+
+    /**
+     * Add exp to player
+     *
+     * @param exp - the exp to add
+     */
+    void addExp(double exp);
+
+    /**
+     * Remove exp from player
+     *
+     * @param exp - the exp to remove
+     */
+    void removeExp(double exp);
+
+    /**
+     * @param skill - the skill type
+     * @return the skill level
+     */
+    int getSkillLevel(Skill skill);
+
+    /**
+     * Define new skill level
+     *
+     * @param skill - the skill type
+     * @param level - the new level
+     */
+    void setSkillLevel(Skill skill, int level);
+
+    /**
+     * @return the current upgrade points
+     */
+    int getUpgradePoints();
+
+    /**
+     * Define new upgrade points to player
+     *
+     * @param upgradePoints - upgrade points amount
+     */
+    void setUpgradePoints(int upgradePoints);
+
+    /**
+     * Add upgrade points to player
+     *
+     * @param upgradePoints - upgrade points amount
+     */
+    void addUpgradePoints(int upgradePoints);
+
+    /**
+     * Remove upgrade points from player
+     *
+     * @param upgradePoints - upgrade points amount.
+     */
+    void removeUpgradePoints(int upgradePoints);
+
+    /**
+     * Upgrade skill level
+     *
+     * @param skill - the skill type
+     * @return the new level
+     */
+    int upgradeSkill(Skill skill);
+
+}
